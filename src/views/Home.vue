7f686ad3f1cd41dfa9ad0ce705ffc60471f09a16
LapoGini <136312901+LapoGini@users.noreply.github.com>
2023-12-07 10:05:51 +0100

    Implementata eliminazione items da memoria interna ed eliminazione per DB

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,9 +20,14 @@
             </ion-select-option>
           </ion-select>
         </ion-item>
-        <ion-button 
-          expand="block" 
-          :disabled="!permissionsGranted || (selectedClient ? selectedClient.value === null : true)"
+        <ion-button @click="onUserAction">Carica Dati</ion-button>
+
+        <ion-button
+          expand="block"
+          :disabled="
+            !permissionsGranted ||
+            (selectedClient ? selectedClient.value === null : true)
+          "
           @click="proceedToNextRoute"
           class="ion-margin-top"
         >
@@ -59,7 +64,7 @@ import { saveClientsToDB, getClientsFromDB } from "@/services/db_clients.js";
 import { saveCitiesToDB, getCitiesFromDB } from "@/services/db_cities.js";
 import { saveStreetsToDB, getStreetsFromDB } from "@/services/db_streets.js";
 import { saveTagsToDB, getTagsFromDB } from "@/services/db_tags.js";
-import { Filesystem } from "@capacitor/filesystem";
+import { Filesystem, Directory } from "@capacitor/filesystem";
 
 const { networkStatus, logCurrentNetworkStatus, showToastBackground } =
   useNetwork();
@@ -69,6 +74,7 @@ const clients = ref([]);
 const cities = ref([]);
 const streets = ref([]);
 const tags = ref([]);
+const deletable = ref([]);
 const selectedClient = ref(null);
 const permissionsGranted = ref(false);
 
@@ -232,13 +238,162 @@ const fetchTags = async () => {
   }
 };
 
+// CHIAMATA PER PRENDERE VEDERE QUALI ITEMS SONO CANCELLABILI
+const fetchDeletable = async () => {
+  try {
+    const apiToken = store.getters.getApiToken;
+    const response = await axios.post(
+      "https://rainwaterdrains.inyourlife.com/api/delete_caditoie",
+      {
+        data: {
+          id_user: localStorage.getItem("user"),
+          iduserhash: localStorage.getItem("apiToken"),
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+        },
+      }
+    );
+    deletable.value = response.data.cancellabili;
+    console.log("deletable", deletable.value);
+    
+    await processDeletableItems();
+  } catch (error) {
+    console.error("Errore durante il recupero dei dati:", error);
+    // Gestisci l'errore
+  }
+};
+// Funzione che esegue il processo di eliminazione per ogni item
+const processDeletableItems = async () => {
+  for (let id of deletable.value) {
+    try {
+      await deleteItemFile(id);
+      console.log(`Item con ID: ${id} eliminato con successo.`);
+    } catch (error) {
+      console.error(`Errore durante l'eliminazione dell'item con ID: ${id}`, error);
+    }
+  }
+};
+// estrae la data dall'id_da_app
+function extractDateFromId(id: any) {
+  const timestamp = id.split('_')[0];
+  const date = new Date(parseInt(timestamp));
+  return formatDate(date);
+}
+// formatta la data per trovare il nome della cartella
+function formatDate(date: any) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hour = date.getHours().toString().padStart(2, '0');
+  return `${year}_${month}_${day}_${hour}`;
+}
+//CHIAMATA PER CERCARE IL FILE DETRO LA CARTELLA ED ELIMINARLO (in caso eliminando pure la cartella)
+async function deleteItemFile(id: any) {
+  const folderName = extractDateFromId(id);
+  console.log('Inizio deleteItemFile - folderName:', folderName);
+  const fileName = `${id}.json`;
+  console.log('fileName:', fileName);
+
+  try {
+    console.log('Tentativo di lettura directory...');
+
+    // Controlla l'esistenza della cartella
+    const directory = await Filesystem.readdir({
+      path: `items/${folderName}`,
+      directory: Directory.Documents,
+    });
+    console.log('Contenuto della directory:', directory);
+
+    // Assicurati che directory.files sia un array di stringhe
+    if (Array.isArray(directory.files)) {
+      console.log('directory.files è un array, procedo con la verifica del file');
+
+      // Utilizza 'some' per verificare se il file esiste nella cartella
+      const fileExists = directory.files.some(file => file.name === fileName);
+      if (fileExists) {
+        console.log(`Trovato il file ${fileName}, procedo con l'eliminazione`);
+
+        // Elimina il file
+        await Filesystem.deleteFile({
+          path: `items/${folderName}/${fileName}`,
+          directory: Directory.Documents,
+        });
+        console.log(`File ${fileName} eliminato con successo.`);
+
+        // Ricontrolla la cartella
+        const updatedDirectory = await Filesystem.readdir({
+          path: `items/${folderName}`,
+          directory: Directory.Documents,
+        });
+        console.log('Contenuto aggiornato della directory:', updatedDirectory);
+
+        // Se la cartella è vuota, elimina anche la cartella
+        if (updatedDirectory.files.length === 0) {
+          console.log(`Cartella ${folderName} vuota, procedo con l'eliminazione`);
+          await Filesystem.rmdir({
+            path: `items/${folderName}`,
+            directory: Directory.Documents,
+          });
+          console.log(`Cartella ${folderName} eliminata con successo.`);
+        }
+      } else {
+        console.log(`Il file ${fileName} non esiste nella cartella.`);
+      }
+    } else {
+      console.log('directory.files non è un array, impossibile procedere.');
+    }
+
+    // Chiamata API per segnalare l'eliminazione
+    console.log(`Segnalazione dell'eliminazione dell'elemento con ID: ${id}`);
+    await markItemAsDeleted(id);
+    console.log(`Elemento con ID: ${id} segnalato come eliminato.`);
+  } catch (error) {
+    console.error("Errore durante l'eliminazione del file:", error);
+    console.error("StackTrace:", error.stack);
+  }
+}
+
+// CHIAMATA API PER ELIMINARE METTERE (delated_at) LA CADITOIA ELIMINATA
+async function markItemAsDeleted(id: any) {
+  try {
+    const apiToken = store.getters.getApiToken;
+    await axios.post(
+      'https://rainwaterdrains.inyourlife.com/api/delete_caditoie_id',
+      {
+        data: {
+          caditoia_id: id,
+          id_user: localStorage.getItem("user"),
+          iduserhash: localStorage.getItem("apiToken"),
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+        },
+      }
+    );
+    console.log("Elemento segnato come eliminato:", id);
+  } catch (error) {
+    console.error("Errore durante la segnalazione dell'eliminazione:", error);
+  }
+}
+
+
+const onUserAction = async () => {
+  const apiResponse = await fetchDeletable();
+  console.log("apiResponse", apiResponse);
+};
+
 onMounted(async () => {
   console.log("CLIENTE SELEZIONATO", selectedClient.value === null);
   const notificationsPermission = await requestNotificationsPermissions();
   const cameraPermission = await requestCameraPermissions();
   const locationPermission = await requestLocationPermissions();
   const filesystemPermission = await requestFilesystemPermissions();
-  console.log('filesystemPermission', filesystemPermission);
+  console.log("filesystemPermission", filesystemPermission);
   // Se tutti i permessi sono stati concessi, imposto permissionsGranted su true
   if (
     notificationsPermission &&
@@ -307,7 +462,7 @@ h1 {
   color: white;
 }
 
-ion-item{
+ion-item {
   border-radius: 10px;
 }
 
