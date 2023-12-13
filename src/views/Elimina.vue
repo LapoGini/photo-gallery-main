0ce44fea90ea397164c9d77a3e88077bd880be1e
LapoGini <136312901+LapoGini@users.noreply.github.com>
2023-12-13 12:27:56 +0100

    Corretta eliminazione caditoie ed implementato messaggio successo

--- src/views/Elimina.vue
+++ src/views/Elimina.vue
@@ -4,16 +4,20 @@
       <div class="main-container ion-text-center">
         <h1>Elimina dati da telefono</h1>
       </div>
-        <ion-button @click="onUserAction">Carica Dati</ion-button>
+      <ion-button @click="onUserAction">Elimina caditoie</ion-button>
 
       <div v-if="!networkStatus" class="no-connection-message">
         La funzione si abilita solamente con una connessione stabile.
+      </div>
+      <div v-if="showAlert" class="success-alert">
+        Le caditoie sono state eliminate con successo dal dispositivo e il
+        gestionale è stato aggiornato.
       </div>
       <ion-loading
         class="loading-backdrop"
         v-if="isLoading"
         :isOpen="true"
-        message="Sincronizzazione..."
+        message="Eliminazione..."
       ></ion-loading>
     </ion-content>
     <div class="toast-background" v-if="showToastBackground"></div>
@@ -47,13 +51,26 @@ const { networkStatus, logCurrentNetworkStatus, showToastBackground } =
 const store = useStore();
 const router = useRouter();
 const deletable = ref([]);
+const showAlert = ref(false);
+const allOperationsSuccessful = ref(false);
 
 
-const onUserAction = async () => {
-  const apiResponse = await fetchDeletable();
-  console.log("apiResponse", apiResponse);
+const showSuccessAlert = () => {
+  showAlert.value = true;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 5000); // Nasconde l'alert dopo 5 secondi
 };
 
+const onUserAction = async () => {
+  console.log("Inizio onUserAction");
+  try {
+    await fetchDeletable();
+  } catch (error) {
+    console.error("Errore durante l'azione dell'utente", error);
+  }
+  console.log("Fine onUserAction");
+};
 
 // CHIAMATA PER PRENDERE VEDERE QUALI ITEMS SONO CANCELLABILI
 const fetchDeletable = async () => {
@@ -75,15 +92,16 @@ const fetchDeletable = async () => {
     );
     deletable.value = response.data.cancellabili;
     console.log("deletable", deletable.value);
-    
     await processDeletableItems();
   } catch (error) {
     console.error("Errore durante il recupero dei dati:", error);
-    // Gestisci l'errore
   }
 };
 // Funzione che esegue il processo di eliminazione per ogni item
 const processDeletableItems = async () => {
+    console.log("Inizio processDeletableItems");
+
+  allOperationsSuccessful.value = true;
   const deletableItems = deletable.value;
   for (let i = 0; i < deletableItems.length; i++) {
     const id = deletableItems[i];
@@ -91,7 +109,12 @@ const processDeletableItems = async () => {
       await deleteItemFile(id);
       console.log(`Item con ID: ${id} eliminato con successo.`);
     } catch (error) {
-      console.error(`Errore durante l'eliminazione dell'item con ID: ${id}`, error);
+      console.error(
+        `Errore durante l'eliminazione dell'item con ID: ${id}`,
+        error
+      );
+      allOperationsSuccessful.value = false;
+      break;
     }
 
     // Attendere 200 millisecondi prima della prossima iterazione
@@ -99,45 +122,53 @@ const processDeletableItems = async () => {
       await new Promise((resolve) => setTimeout(resolve, 200));
     }
   }
+  if (allOperationsSuccessful.value) {
+    console.log('ORA');
+    showSuccessAlert();
+  }
+    console.log("Fine processDeletableItems");
+
 };
 
 // estrae la data dall'id_da_app
 function extractDateFromId(id: any) {
-  const timestamp = id.split('_')[0];
+  const timestamp = id.split("_")[0];
   const date = new Date(parseInt(timestamp));
   return formatDate(date);
 }
 // formatta la data per trovare il nome della cartella
 function formatDate(date: any) {
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hour = date.getHours().toString().padStart(2, "0");
   return `${year}_${month}_${day}_${hour}`;
 }
 //CHIAMATA PER CERCARE IL FILE DETRO LA CARTELLA ED ELIMINARLO (in caso eliminando pure la cartella)
 async function deleteItemFile(id: any) {
   const folderName = extractDateFromId(id);
-  console.log('Inizio deleteItemFile - folderName:', folderName);
+  console.log("Inizio deleteItemFile - folderName:", folderName);
   const fileName = `${id}.json`;
-  console.log('fileName:', fileName);
+  console.log("fileName:", fileName);
 
   try {
-    console.log('Tentativo di lettura directory...');
+    console.log("Tentativo di lettura directory...");
 
     // Controlla l'esistenza della cartella
     const directory = await Filesystem.readdir({
       path: `items/${folderName}`,
       directory: Directory.Documents,
     });
-    console.log('Contenuto della directory:', directory);
+    console.log("Contenuto della directory:", directory);
 
     // Assicurati che directory.files sia un array di stringhe
     if (Array.isArray(directory.files)) {
-      console.log('directory.files è un array, procedo con la verifica del file');
+      console.log(
+        "directory.files è un array, procedo con la verifica del file"
+      );
 
       // Utilizza 'some' per verificare se il file esiste nella cartella
-      const fileExists = directory.files.some(file => file.name === fileName);
+      const fileExists = directory.files.some((file) => file.name === fileName);
       if (fileExists) {
         console.log(`Trovato il file ${fileName}, procedo con l'eliminazione`);
 
@@ -153,11 +184,13 @@ async function deleteItemFile(id: any) {
           path: `items/${folderName}`,
           directory: Directory.Documents,
         });
-        console.log('Contenuto aggiornato della directory:', updatedDirectory);
+        console.log("Contenuto aggiornato della directory:", updatedDirectory);
 
         // Se la cartella è vuota, elimina anche la cartella
         if (updatedDirectory.files.length === 0) {
-          console.log(`Cartella ${folderName} vuota, procedo con l'eliminazione`);
+          console.log(
+            `Cartella ${folderName} vuota, procedo con l'eliminazione`
+          );
           await Filesystem.rmdir({
             path: `items/${folderName}`,
             directory: Directory.Documents,
@@ -168,7 +201,7 @@ async function deleteItemFile(id: any) {
         console.log(`Il file ${fileName} non esiste nella cartella.`);
       }
     } else {
-      console.log('directory.files non è un array, impossibile procedere.');
+      console.log("directory.files non è un array, impossibile procedere.");
     }
 
     // Chiamata API per segnalare l'eliminazione
@@ -186,13 +219,13 @@ async function markItemAsDeleted(id: any) {
   try {
     const apiToken = store.getters.getApiToken;
     await axios.post(
-      'https://rainwaterdrains.inyourlife.com/api/delete_caditoie_id',
+      "https://rainwaterdrains.inyourlife.com/api/delete_caditoie_id",
       {
         data: {
           caditoia_id: id,
           id_user: localStorage.getItem("user"),
           iduserhash: localStorage.getItem("apiToken"),
-        }
+        },
       },
       {
         headers: {
@@ -227,7 +260,6 @@ h1 {
   font-weight: bold;
 }
 
-
 .sub-container {
   width: 90%;
   margin: auto;
@@ -257,5 +289,14 @@ ion-button {
   color: white;
   text-align: center;
   padding: 10px;
+}
+
+.success-alert {
+  color: white;
+  text-align: center;
+  padding: 10px;
+  background-color: #4caf50;
+  margin-top: 20px;
+  border-radius: 10px;
 }
 </style>
